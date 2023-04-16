6c5153d77d1eb977220f24c34eacac79048b3c95
EgRoku <jimenezjj5150@gmail.com>
2023-04-15 23:14:01 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function generatePassword () {
+  
+}
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//dont touch code above
+//start with defining generate password (above)
